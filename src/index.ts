0717cc099af239c98f161effa4a65f3e12a18cee
Artem Volodin <Volodin.artemka@gmail.com>
2026-08-07 21:28:36 +0200

    fix: Some fixes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
+import taskRouter from './routes/task.router';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server is running!');
-});
+app.use('/api/tasks', taskRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
